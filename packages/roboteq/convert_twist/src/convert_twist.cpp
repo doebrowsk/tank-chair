177abaab7f10efa503ed8b605ef9ac60fb88b77c
rkyuan <hxy153@case.edu>
2016-03-17 14:11:57 -0400

    calibrated the motor driver

--- packages/roboteq/convert_twist/src/convert_twist.cpp
+++ packages/roboteq/convert_twist/src/convert_twist.cpp
@@ -3,22 +3,32 @@
 #include <std_msgs/Float64.h>
 #include <roboteq_msgs/Command.h>
 
-
+//this node takes a geometry msgs twist in m/s and rad/s
+//and publishes roboteq_msgs to the 2 channels
 ros::Publisher ch1, ch2;
-float MAGIC_NUM = 0.833;
+
+float MAX_COMMAND = 205.0;
+float MAX_SPEED = 1.22;
+float BASE_RADIUS = 0.3;
+
 void myCallback(const geometry_msgs::Twist& message_holder) 
 {
 	float v1,v2;
-	v2 += message_holder.angular.z/2.0;
-	v1 -= message_holder.angular.z/2.0;
+	
 	v1 += message_holder.linear.x;
 	v2 += message_holder.linear.x;
+
+	v1*=MAX_COMMAND/MAX_SPEED;
+	v2*=MAX_COMMAND/MAX_SPEED;
+
+	v2 += (message_holder.angular.z/2.0)*MAX_COMMAND/MAX_SPEED*BASE_RADIUS;
+	v1 -= (message_holder.angular.z/2.0)*MAX_COMMAND/MAX_SPEED*BASE_RADIUS;
+	v1 = std::max(-1.0f*MAX_COMMAND,std::min(v1,MAX_COMMAND));
+	v2 = std::max(-1.0f*MAX_COMMAND,std::min(v2,MAX_COMMAND));
 
 	roboteq_msgs::Command cmd1, cmd2;
 	cmd1.mode = 0;
 	cmd2.mode = 0;
-	v1*= MAGIC_NUM;
-	v2*= MAGIC_NUM;
 	cmd1.setpoint = v1;
 	cmd2.setpoint = v2;
 
--- packages/roboteq/roboteq_driver/src/channel.cpp
+++ packages/roboteq/roboteq_driver/src/channel.cpp
@@ -59,9 +59,11 @@ void Channel::cmdCallback(const roboteq_msgs::Command& command)
 
   if (command.mode == roboteq_msgs::Command::MODE_VELOCITY)
   {
+    //ROS_INFO("vel");
     // Get a -1000 .. 1000 command as a proportion of the maximum RPM.
-    int roboteq_velocity = to_rpm(command.setpoint) / max_rpm_ * 1000.0;
+    int roboteq_velocity = command.setpoint;
     ROS_DEBUG_STREAM("Commanding " << roboteq_velocity << " velocity to motor driver.");
+
 
     // Write mode and command to the motor driver.
     controller_->command << "G" << channel_num_ << roboteq_velocity << controller_->send;
