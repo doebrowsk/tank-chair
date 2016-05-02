d3d9dfcc3e13446bef86c86d3b475a3a8d9cfc4f
rkyuan <hxy153@case.edu>
2016-05-02 18:13:04 -0400

    bounds

--- packages/sentry/gps_bug/src/scan_expander.cpp
+++ packages/sentry/gps_bug/src/scan_expander.cpp
@@ -17,9 +17,11 @@ void scancb(const sensor_msgs::LaserScan& message_holder){
 			}
 			float horizon = sqrt(pow(distance,2)-pow(radius,2));
 			float scan_ang_size = acos(horizon/distance);
-			float circle_ang_size = 90 - scan_ang_size;
 			float scan_ang_diff = abs(scantofill-compare)*cspace_scan.angle_increment;
-			float cspace_range = (distance-radius)*(1+1/cos(scan_ang_diff));// distance to the line tangent to the circle
+			if (scan_ang_diff>scan_ang_size) continue;
+			
+			float circle_ang_size = 90 - scan_ang_size;
+			float cspace_range = (distance-radius)/cos(scan_ang_diff);// distance to the line tangent to the circle
 			if (cspace_range<0) ROS_INFO("dist to line neg");
 			cspace_range+= radius-cos(circle_ang_size*scan_ang_diff/scan_ang_size);
 			if (cspace_range<0) ROS_INFO("total neg");
