afb15ff333f286474054ff7a9226872b279e4600
rkyuan <hxy153@case.edu>
2016-05-02 19:58:49 -0400

    that was a bad idea

--- packages/sentry/gps_bug/src/scan_expander.cpp
+++ packages/sentry/gps_bug/src/scan_expander.cpp
@@ -3,10 +3,34 @@
 #include <math.h>
 
 ros::Publisher scanpub;
-sensor_msgs::LaserScan message_holder;
 
-void scancb(const sensor_msgs::LaserScan& input){
-	message_holder = input;
+void scancb(const sensor_msgs::LaserScan& message_holder){
+	float radius = 0.7;//expansion of each scan
+	sensor_msgs::LaserScan cspace_scan = message_holder;
+	cspace_scan.range_min = 0;
+	for (int scantofill = 0; scantofill < (message_holder.angle_max - message_holder.angle_min)/message_holder.angle_increment;scantofill++){
+		for (int compare = 0; compare < (message_holder.angle_max - message_holder.angle_min)/message_holder.angle_increment;compare++){
+			float distance = message_holder.ranges[compare];
+			if (distance < radius){
+				cspace_scan.ranges[scantofill]=0.0;
+				continue;
+			}
+			float horizon = sqrt(pow(distance,2)-pow(radius,2));
+			float scan_ang_size = acos(horizon/distance);
+			float scan_ang_diff = abs(scantofill-compare)*cspace_scan.angle_increment;
+			if (scan_ang_diff>scan_ang_size) continue;
+			float circle_ang_size = 90 - scan_ang_size;
+			float cspace_range = (distance-radius)/cos(scan_ang_diff);// distance to the line tangent to the circle
+			if (cspace_range<0) ROS_INFO("dist to line neg");
+			cspace_range+= radius*(1-cos(circle_ang_size*scan_ang_diff/scan_ang_size));
+			if (cspace_range<0) ROS_INFO("total neg");
+			if (cspace_range<cspace_scan.ranges[scantofill]){
+				cspace_scan.ranges[scantofill]=cspace_range;
+			}
+
+		}
+	}
+	scanpub.publish(cspace_scan);
 }
 
 int main(int argc, char **argv) {
@@ -15,35 +39,6 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh;
     scanpub = nh.advertise<sensor_msgs::LaserScan>("gps_bug/cspace_scan", 1);
     ros::Subscriber scansub = nh.subscribe("scan",1,scancb);
-
-    while(ros::ok()){
-	    ros::spinOnce();
-	    float radius = 0.7;//expansion of each scan
-		sensor_msgs::LaserScan cspace_scan = message_holder;
-		cspace_scan.range_min = 0;
-		for (int scantofill = 0; scantofill < (message_holder.angle_max - message_holder.angle_min)/message_holder.angle_increment;scantofill++){
-			for (int compare = 0; compare < (message_holder.angle_max - message_holder.angle_min)/message_holder.angle_increment;compare++){
-				float distance = message_holder.ranges[compare];
-				if (distance < radius){
-					cspace_scan.ranges[scantofill]=0.0;
-					continue;
-				}
-				float horizon = sqrt(pow(distance,2)-pow(radius,2));
-				float scan_ang_size = acos(horizon/distance);
-				float scan_ang_diff = abs(scantofill-compare)*cspace_scan.angle_increment;
-				if (scan_ang_diff>scan_ang_size) continue;
-				float circle_ang_size = 90 - scan_ang_size;
-				float cspace_range = (distance-radius)/cos(scan_ang_diff);// distance to the line tangent to the circle
-				if (cspace_range<0) ROS_INFO("dist to line neg");
-				cspace_range+= radius*(1-cos(circle_ang_size*scan_ang_diff/scan_ang_size));
-				if (cspace_range<0) ROS_INFO("total neg");
-				if (cspace_range<cspace_scan.ranges[scantofill]){
-					cspace_scan.ranges[scantofill]=cspace_range;
-				}
-
-			}
-		}
-		scanpub.publish(cspace_scan);
-	}
+    ros::spin();
 
 }
