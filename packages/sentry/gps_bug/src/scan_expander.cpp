5387c307a01fd0faece3d9b192f8801c94fd229b
rkyuan <hxy153@case.edu>
2016-05-02 18:29:29 -0400

    fix

--- packages/sentry/gps_bug/src/scan_expander.cpp
+++ packages/sentry/gps_bug/src/scan_expander.cpp
@@ -5,7 +5,7 @@
 ros::Publisher scanpub;
 
 void scancb(const sensor_msgs::LaserScan& message_holder){
-	float radius = 1.3;//expansion of each scan
+	float radius = 0.7;//expansion of each scan
 	sensor_msgs::LaserScan cspace_scan = message_holder;
 	cspace_scan.range_min = 0;
 	for (int scantofill = 0; scantofill < (message_holder.angle_max - message_holder.angle_min)/message_holder.angle_increment;scantofill++){
@@ -19,11 +19,10 @@ void scancb(const sensor_msgs::LaserScan& message_holder){
 			float scan_ang_size = acos(horizon/distance);
 			float scan_ang_diff = abs(scantofill-compare)*cspace_scan.angle_increment;
 			if (scan_ang_diff>scan_ang_size) continue;
-			
 			float circle_ang_size = 90 - scan_ang_size;
 			float cspace_range = (distance-radius)/cos(scan_ang_diff);// distance to the line tangent to the circle
 			if (cspace_range<0) ROS_INFO("dist to line neg");
-			cspace_range+= radius-cos(circle_ang_size*scan_ang_diff/scan_ang_size);
+			cspace_range+= radius*(1-cos(circle_ang_size*scan_ang_diff/scan_ang_size));
 			if (cspace_range<0) ROS_INFO("total neg");
 			if (cspace_range<cspace_scan.ranges[scantofill]){
 				cspace_scan.ranges[scantofill]=cspace_range;
