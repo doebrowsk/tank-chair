636056776d6e19bdb1026d60aaca1652b195c746
rkyuan <hxy153@case.edu>
2016-05-02 23:59:03 -0400

    fixed?

--- packages/sentry/gps_bug/src/scan_expander.cpp
+++ packages/sentry/gps_bug/src/scan_expander.cpp
@@ -10,8 +10,7 @@ void scancb(const sensor_msgs::LaserScan& message_holder){
 	cspace_scan.range_min = 0;
 	for (int scantofill = 0; scantofill < (message_holder.angle_max - message_holder.angle_min)/message_holder.angle_increment;scantofill++){
 		for (int compare = 0; compare < (message_holder.angle_max - message_holder.angle_min)/message_holder.angle_increment;compare++){
-			float distance = 8;
-			if (compare==90) distance = 3;
+			float distance = message_holder.ranges[compare];
 			if (distance < radius){
 				cspace_scan.ranges[scantofill]=0.0;
 				continue;
@@ -20,11 +19,10 @@ void scancb(const sensor_msgs::LaserScan& message_holder){
 			float scan_ang_size = acos(horizon/distance);
 			float scan_ang_diff = abs(scantofill-compare)*cspace_scan.angle_increment;
 			if (scan_ang_diff>scan_ang_size) continue;
-			float circle_ang_size = 90 - scan_ang_size;
+			float circle_ang_size = 1.5708 - scan_ang_size;
 			float cspace_range = (distance-radius)/cos(scan_ang_diff);// distance to the line tangent to the circle
 			if (cspace_range<0) ROS_INFO("dist to line neg");
 			cspace_range+= radius*(1-cos(circle_ang_size*scan_ang_diff/scan_ang_size));
-			if (compare>75 && compare < 105) ROS_INFO("cang %f, sang_diff %f, sang %f,"circle_ang_size, scan_ang_diff, scan_ang_size);
 			if (cspace_range<0) ROS_INFO("total neg");
 			if (cspace_range<cspace_scan.ranges[scantofill]){
 				cspace_scan.ranges[scantofill]=cspace_range;
